7adaba636829b228a3b7f4fcd34f9b7b0f124d79
lejodev <alejandro.rv97@gmail.com>
2021-06-18 22:08:03 -0500

    Home static part finished

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import "./_home.scss";
 import "../../styles/buttons/_my-button.scss";
 import { BsArrowRight } from "react-icons/bs";
+import codingImage from "../../images/coding.jpeg";
 
 const Home = () => {
   return (
-    <div className="home">
+    <div className="home" style={
+        {backgroundImage:`url(${codingImage})`}
+    }>
+      <div className="text">
+        <span className="text-row">
+          Hi everyone. My name is Alejandro Rincon Vera.
+        </span>
+        <span className="text-row">
+          I'm a full stack web developer passionate about
+        </span>
+        <span className="text-row">building new stuff through software</span>
+      </div>
       <div className="button">
-        <div className="text">See my work</div>
+        See my work
         <div className="icon">
-          <BsArrowRight className="iconn"  size={40} />
+          <BsArrowRight className="iconn" size={40} />
         </div>
       </div>
     </div>
