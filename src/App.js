381198bf8f0e4a47d8f87caebe455c921c173663
lejodev <alejandro.rv97@gmail.com>
2021-11-20 20:48:11 -0500

    non used files deleted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./components/nav/Navigation.jsx";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
