490ebb33818519e4c44d219b8c7190f0977cb4f4
lejodev <alejandro.rv97@gmail.com>
2021-11-22 17:11:06 -0500

    Portfolio continuation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./_contact.scss";
 
 const Contact = () => {
-  return <div className="contact" id="contact">contact</div>;
+  return <div className="contact" id="contact">
+    <h1 className="contact">Contact</h1>
+  </div>;
+
 };
 
 export default Contact;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,23 +7,27 @@ import gifos1 from "../../images/gifos/gifos1.PNG";
 const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
-      <Project
-        image={p1}
-        title="Project 1"
-        projectDescription="Static, responsive vanilla css landing page for a podcast service with a burger menu"
-        projectLink="https://github.com/lejodev/delivery-services-web-app"
-      />
-      <Project
-        image={gifos1}
-        title="GIFOS"
-        projectDescription="Responsive frontend app that allows user to see, create, set and delete favorites gifs, algo set page with light/dark mode. This page was done using sass and using the gifos API"
-        projectLink="https://github.com/lejodev/delivery-services-web-app"
-      />
-      <Project
-        title="Services"
-        projectDescription="Consequuntur placeat natus sapiente laudantium repudiandae quasi rerum corrupti, nisi, doloremque exercitationem debitis magnam quaerat vero doloribus cum maiores voluptates quidem dolore assumenda cumque. Vero ullam aliquid vel velit."
-        projectLink="https://github.com/lejodev/delivery-services-web-app"
-      />
+      <div className="projects-title">Projects</div>
+      <section className="projects">
+        <Project
+          image={p1}
+          title="Project 1"
+          projectDescription="Static, responsive vanilla css landing page for a podcast service with a burger menu"
+          projectLink="https://github.com/lejodev/Acamica-project1"
+        />
+        <Project
+          image={gifos1}
+          title="GIFOS"
+          projectDescription="Responsive frontend app that allows user to see, create, set and delete favorite gifs, algo set page with light/dark mode. This page was done using sass and using the gifos API"
+          projectLink="https://github.com/lejodev/Acamica-project2-GIFOS"
+        />
+        <Project
+          title="Services"
+          projectDescription=" ** Working on ** Full stack app that lets user choose an hour to get a service "
+          projectLink="https://github.com/lejodev/delivery-services-web-app"
+        />
+        
+      </section>
     </div>
   );
 };
